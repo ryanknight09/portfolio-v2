2c53b7c033eb4373da1982056216280180c59201
Ryan Knight <ryan.knight09@outlook.com>
2023-06-29 15:49:04 -0700

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
@@ -18,19 +19,11 @@ export default defineConfig({
 
   plugins: [
     react(),
+    svgr({}),
     viteTsConfigPaths({
       root: './',
     }),
   ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: './',
-  //    }),
-  //  ],
-  // },
 
   test: {
     globals: true,
