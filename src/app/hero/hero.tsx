1f2f43f29233bdc90f40c76fcfbfc2c53c90977a
Ryan Knight <ryan.knight09@outlook.com>
2023-06-30 13:12:02 -0700

    grid layout

--- src/app/hero/hero.tsx
+++ src/app/hero/hero.tsx
@@ -9,10 +9,11 @@ import {
 
 import { ReactComponent as GithubIcon } from '../../assets/images/icon-github.svg';
 import { ReactComponent as LinkedInIcon } from '../../assets/images/icon-linkedin.svg';
+import ProfileImage from '../../assets/images/image-profile-desktop.webp';
 
 const Hero = () => (
   <HeroWrapper>
-    <SpaceBetween>
+    <HeadingContent>
       <Caption>ryanknight</Caption>
       <Icons>
         <IconButton color="primary">
@@ -22,36 +23,55 @@ const Hero = () => (
           <GithubIcon />
         </IconButton>
       </Icons>
-    </SpaceBetween>
-    <Typography variant="h1">Nice to meet you!</Typography>
-    <Name>
-      I'm <span>Ryan Knight</span>.
-    </Name>
-    <Typography variant="body1" mt="3rem" mb="4rem" maxWidth="27rem">
-      Based in Spokane Washington, I’m a front-end developer passionate about
-      building accessible web apps that users love.
-    </Typography>
-    <Button>Contact Me</Button>
+    </HeadingContent>
+    <Content>
+      <Greeting>
+        Nice to meet you! I'm <span>Ryan Knight</span>.
+      </Greeting>
+      <About>
+        Based in Spokane Washington, I’m a front-end developer passionate about
+        building accessible web apps that users love.
+      </About>
+      <Button sx={{ marginTop: '2.25rem' }}>Contact Me</Button>
+    </Content>
+    <ProfileImg src={ProfileImage} alt="profile-image" />
   </HeroWrapper>
 );
 
 export default Hero;
 
-const HeroWrapper = styled(Stack)(({ theme }) => ({
+const overLapGrid = '1/2';
+
+const HeroWrapper = styled(Box)(({ theme }) => ({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(9, 1fr)',
+  width: '100%',
+  zIndex: 10,
+}));
+
+const HeadingContent = styled(Box)(() => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'flex-start',
+  gridRow: overLapGrid,
+  gridColumn: '1/-1',
+  zIndex: 10,
   paddingTop: '2.5rem',
+}));
+
+const Content = styled(Stack)(() => ({
+  marginTop: '4rem',
+  gap: '2.5rem',
+  zIndex: 10,
+  gridColumn: '1/7',
+  gridRow: overLapGrid,
+  alignSelf: 'center',
 }));
 
 const Icons = styled(Box)(({ theme }) => ({
   display: 'flex',
   gap: '1rem',
-}));
-
-const SpaceBetween = styled(Box)(({ theme }) => ({
-  width: '100%',
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  marginBottom: '8rem',
+  marginRight: '1rem',
 }));
 
 const Caption = styled(Typography)(({ theme }) => ({
@@ -60,9 +80,22 @@ const Caption = styled(Typography)(({ theme }) => ({
   lineHeight: '2rem',
 }));
 
-const Name = styled(Typography)(({ theme }) => ({
+const Greeting = styled(Typography)(({ theme }) => ({
   ...theme.typography.h1,
   span: {
     borderBottom: `.375rem solid ${theme.palette.success.main}`,
   },
+}));
+
+const About = styled(Typography)(({ theme }) => ({
+  ...theme.typography.body1,
+  width: '27rem',
+}));
+
+const ProfileImg = styled('img')(({ theme }) => ({
+  marginLeft: 'auto',
+  width: '27rem',
+  gridColumn: '5/-1',
+  gridRow: overLapGrid,
+  zIndex: 1,
 }));
